4b11073308e0d8a77169516f166a7bbe38b28684
upendrakumar <krupendra7725@gmail.com>
2026-09-24 01:07:41 +0530

    Fix production CORS

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin : "http://localhost:5175",
+  origin : "https://prepify-frontend-xtu3.onrender.com",
   credentials: true,
 }));
 
